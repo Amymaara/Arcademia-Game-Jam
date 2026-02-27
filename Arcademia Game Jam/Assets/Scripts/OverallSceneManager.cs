1181a410d348626c3828b827725b8ddbe11239b0
afancyfrog <tanyafreese@gmail.com>
2026-02-27 22:52:31 +0200

    startingsetup

---
+++ Arcademia Game Jam/Assets/Scripts/OverallSceneManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class OverallSceneManager : MonoBehaviour
+{
+    /* 
+     This is going to control the flow of the game
+
+      things referenced here
+    - the party of the player
+    - what areas still need to be encountered
+    - loads in new battles on choice buttons
+
+     */
+
+    public void OnHeavenChoice()
+    {
+        // load in heaven battle group
+        //disable dialogue
+    }
+
+    public void OnUnderworldChoice()
+    {
+        // load in underworld battle group
+        //disable dialogue
+    }
+
+    public void OnEarthChoice()
+    {
+        // load in earth battle group
+        // disable dialogue
+    }
+
+}
---
+++ Arcademia Game Jam/Assets/Scripts/Unit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string creatureName;
+
+    public int basicDamage;
+    // include special attack info here too
+    
+
+    public int maxHP;
+    public int currentHP;
+}
